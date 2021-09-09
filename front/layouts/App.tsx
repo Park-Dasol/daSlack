c6e67548c2c871ecb8259047f5b05747a1e5960b
dryans1 <dryans@naver.com>
2021-09-09 18:08:18 +0900

    initial setting

---
+++ front/layouts/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import loadable from "@loadable/component"
+import {Switch, Route, Redirect} from 'react-router-dom'
+
+const LogIn = loadable(()=> import('@pages/LogIn'))
+const SignUp = loadable(()=> import('@pages/SignUp'))
+
+const App = () => {
+  return (
+    <Switch> 
+      <Redirect exact path="/" to="/login"/>
+      <Route path="/login" component={LogIn}/>
+      <Route path="/signup" component={SignUp}/>
+    </Switch>
+  )
+}
+
+// switch : 여러개 라우터 중에 하나만 화면에 표시해주는것
+// redirect : 다른 페이지로 돌려주는 역할
+// route : 컴포넌트를 화면에 띄워주는 역할
+
+
+export default App;
