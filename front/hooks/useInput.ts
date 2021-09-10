fbc1408faf5a151ec314efbb383326d81b5d0905
dryans1 <dryans@naver.com>
2021-09-10 23:50:39 +0900

    feat : signup 페이지 커스텀 훅으로 useInput만들어서 적용 및 회원가입 api응답별로 성공 실패 메시지 구현

---
+++ front/hooks/useInput.ts
@@ -0,0 +1,16 @@
+import { useCallback,useState, Dispatch, SetStateAction, ChangeEvent } from "react"
+
+//react에서 제공하는 hook을 합쳐서 새로운 하나의 hook을 만든것 => custom hook
+
+type ReturnTypes<T = any> = [T, (e: ChangeEvent<HTMLInputElement>) => void, Dispatch<SetStateAction<T>>]
+// typescript의 장점 가독성은 조금 떨어지나 안정성이 높아짐
+
+const useInput = <T = any>(InitialData :T) : ReturnTypes<T> => {
+  const [value, setValue] = useState(InitialData)
+  const handler = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setValue((e.target.value as unknown) as T)
+  },[])
+  return [value, handler, setValue]
+}
+
+export default useInput;
--- front/pages/SignUp/index.tsx
+++ front/pages/SignUp/index.tsx
@@ -1,17 +1,54 @@
+
+import useInput from '@hooks/useInput';
 import React , {useState, useCallback} from 'react'
-import {Form, Label, Input, LinkContainer, Button, Header} from './styles'
+import {Success, Form,Error,  Label, Input, LinkContainer, Button, Header} from './styles'
+import axios from 'axios'
+
 
 const SignUp = () => {
 
-  const [email] = useState('');
-  const [nickname] = useState('');
-  const [password] = useState('');
-  const [passwordCheck] = useState('');
-  const onChangeEmail = useCallback(()=>{}, []);
-  const onChangeNickname = useCallback(()=>{}, []);
-  const onChangePassword= useCallback(()=>{}, []);
-  const onChangePasswordCheck= useCallback(()=>{}, []);
-  const onSubmit= useCallback(()=>{}, []);
+  const [email,onChangeEmail] = useInput('');
+  const [nickname, onChangeNickname] =  useInput('');
+  const [password, , setPassword] = useInput('');
+  const [passwordCheck, , setPasswordCheck] = useInput('');
+  const [signUpError, setSignUpError] = useState('');
+  const [signUpSuccess, setSignUpSuccess] = useState(false);
+  
+
+  // errors
+  const [mismatchError,  ,setMismatchError] = useInput(false);
+
+  const onChangePassword= useCallback((e)=>{
+    setPassword(e.target.value)
+    setMismatchError(e.target.value !== passwordCheck)
+  }, [passwordCheck]);
+
+  const onChangePasswordCheck= useCallback((e)=>{
+    setPasswordCheck(e.target.value)
+    setMismatchError(e.target.value !== password)
+  }, [password]); // 함수 기준으로 외부 변수일때만 써준다. 함수바깥에서 선언한애들만 쓴다.
+  // setPassword 같은것도 외부변수나 외부함수로 생각하지만 공식문서에 따르면 한번 선언하면 바뀌지 않는다고 한다.
+
+  const onSubmit= useCallback((e)=>{
+    e.preventDefault();
+    setSignUpError('');
+    setSignUpSuccess(false);
+    console.log(email, nickname, password,passwordCheck)
+    if (!mismatchError) {
+      console.log('서버로 회원가입하기');
+      axios.post('/api/users', {
+          email, nickname, password,
+        }).then((response) => {
+          console.log(response)
+          setSignUpSuccess(true);
+        })
+        .catch((error)=>{
+          console.log(error.reponse)
+          setSignUpError(error.response.data)
+        })
+        .finally(()=> {})
+    }
+  }, [email, nickname, password,   passwordCheck, mismatchError]);
 
 
   return (
@@ -47,10 +84,10 @@ const SignUp = () => {
               onChange={onChangePasswordCheck}
             />
           </div>
-          {/* {mismatchError && <Error>비밀번호가 일치하지 않습니다.</Error>}
+          {mismatchError && <Error>비밀번호가 일치하지 않습니다.</Error>}
           {!nickname && <Error>닉네임을 입력해주세요.</Error>}
           {signUpError && <Error>{signUpError}</Error>}
-          {signUpSuccess && <Success>회원가입되었습니다! 로그인해주세요.</Success>} */}
+          {signUpSuccess && <Success>회원가입되었습니다! 로그인해주세요.</Success>}
         </Label>
         <Button type="submit">회원가입</Button>
       </Form>
