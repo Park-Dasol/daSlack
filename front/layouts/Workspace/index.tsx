5a54b8a3084fa85f0c84d972ffc5a52ebb70c5d8
dryans1 <dryans@naver.com>
2021-09-17 01:53:43 +0900

    feat : 워크스페이스 생성 모달

--- front/layouts/Workspace/index.tsx
+++ front/layouts/Workspace/index.tsx
@@ -3,6 +3,7 @@ import React, {FC, useCallback, useState} from 'react'
 import useSWR from 'swr'
 import fetcher from '@utils/fetcher'
 import { Redirect, Switch, Route } from 'react-router'
+import { Link } from 'react-router-dom'
 import {
   AddButton,
   Channels,
@@ -22,6 +23,12 @@ import {
 import gravatar from 'gravatar'
 import loadable from '@loadable/component'
 import Menu from '@components/Menu'
+import { IUser } from '@typings/db'
+import { Button, Input, Label } from '@pages/SignUp/styles'
+import useInput from '@hooks/useInput'
+import Modal from '@components/Modal'
+import {toast} from 'react-toastify'
+
 
 const Channel = loadable(()=> import('@pages/Channel'))
 const DirectMessage = loadable(()=> import('@pages/DirectMessage'))
@@ -29,10 +36,14 @@ const DirectMessage = loadable(()=> import('@pages/DirectMessage'))
 
 
 const Workspace:FC = ({children})=> {
-  const { data, error, revalidate, mutate} = useSWR('/api/users', fetcher, {
+  const { data : userData, error, revalidate, mutate} = useSWR<IUser | false>('/api/users', fetcher, {
     dedupingInterval: 2000, // 2초
   })
   const [showUserMenu, setShowUserMenu] = useState(false)
+  const [showCreateWorkspaceModal, setShowCreateWorkspaceModal] = useState(false)
+  const [newWorkspace, onChangeNewWorkspace, setNewWorkspace] = useInput('')
+  const [newUrl, onChangeNewUrl, setNewUrl] = useInput('')
+
 
   const onLogout = useCallback(() => {
     axios.post('/api/users/logout', null, {
@@ -44,11 +55,44 @@ const Workspace:FC = ({children})=> {
   }, [])
 
   // 토글함수
-  const onClickUserProfile = useCallback(() => {
+  const onClickUserProfile = useCallback((e) => {
+    e.stopPropagation()
     setShowUserMenu((prev) => !prev) // 
   }, [])
 
-  if (!data) { // return은 항상 hooks들 보다 아래에 있어야만 에러가 나지 않는다.
+  const onClickCreateWorkspace = useCallback(()=> {
+    setShowCreateWorkspaceModal(true)
+  }, [])
+
+  const onCreateWorkspace = useCallback((e)=> {
+    e.preventDefault();
+    if (!newWorkspace|| !newWorkspace.trim()) return;
+    if (!newUrl|| !newUrl.trim()) return;
+    axios.post('/api/workspace', {
+      workspace :newWorkspace,
+      url: newUrl,
+    }, {
+      withCredentials: true,
+    })
+    .then(()=> {
+      revalidate();
+      setShowCreateWorkspaceModal(false)
+      setNewWorkspace('')
+      setNewUrl('')
+    })
+    .catch((error)=> {
+      console.log(error)
+      toast.error(error.response?.data, { position: 'bottom-center' });
+    })
+
+  }, [newWorkspace, newUrl])
+
+
+  const onCloseModal = useCallback(()=> {
+    setShowCreateWorkspaceModal(false)
+  }, [])
+
+  if (!userData) { // return은 항상 hooks들 보다 아래에 있어야만 에러가 나지 않는다.
     return <Redirect to="/login"/>
   }
 
@@ -58,13 +102,13 @@ const Workspace:FC = ({children})=> {
       <Header>
         <RightMenu>
           <span onClick={onClickUserProfile}>
-          <ProfileImg src={gravatar.url(data.email, {s : '28px', d: 'retro'})} alt={data.nickname}/>
+          <ProfileImg src={gravatar.url(userData.email, {s : '28px', d: 'retro'})} alt={userData.nickname}/>
           {showUserMenu && (
             <Menu style={{ right: 0, top: 38}} show={showUserMenu} onCloseModal={onClickUserProfile}>
               <ProfileModal>
-                <img src={gravatar.url(data.email, {s : '28px', d: 'retro'})} alt={data.nickname}/>
+                <img src={gravatar.url(userData.email, {s : '28px', d: 'retro'})} alt={userData.nickname}/>
                 <div>
-                  <span id="profile-name">{data.nickname}</span>
+                  <span id="profile-name">{userData.nickname}</span>
                   <span id="profile-active">Active</span>
                 </div>
               </ProfileModal>
@@ -75,7 +119,16 @@ const Workspace:FC = ({children})=> {
         </RightMenu>
       </Header>
       <WorkspaceWrapper>
-        <Workspaces>worksppace</Workspaces>
+        <Workspaces>
+          {userData?.Workspaces.map((ws) => {
+          return (
+            <Link key={ws.id} to={`/workspace/${123}/channel/일반`}>
+              <WorkspaceButton>{ws.name.slice(0,1).toUpperCase()}</WorkspaceButton>
+            </Link>
+          )
+        })}
+          <AddButton onClick={onClickCreateWorkspace}>+</AddButton>
+        </Workspaces>
         <Channels>
           <WorkspaceName>workspacename</WorkspaceName>
           <MenuScroll>menuscroll</MenuScroll>
@@ -87,6 +140,19 @@ const Workspace:FC = ({children})=> {
            </Switch>
          </Chats> 
       </WorkspaceWrapper>
+      <Modal show={showCreateWorkspaceModal} onCloseModal={onCloseModal}>
+        <form onSubmit={onCreateWorkspace}>
+          <Label id="workspace-label">
+            <span>워크스페이스이름</span>
+            <Input id="workspace" value={newWorkspace} onChange={onChangeNewWorkspace}/>
+          </Label>
+          <Label id="workspace-url-label">
+            <span>워크스페이스 url</span>
+            <Input id="workspace" value={newUrl} onChange={onChangeNewUrl}/>
+          </Label>
+          <Button type="submit">생성하기</Button>
+        </form>
+      </Modal>
     </div>
   )
 }
