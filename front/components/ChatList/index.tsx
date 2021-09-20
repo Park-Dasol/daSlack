c7dca58a15c3dc830582da57c165d13f335312c0
dryans1 <dryans@naver.com>
2021-09-21 00:43:57 +0900

    feat : 커스텀 스크롤 추가와 dayjs 추가

--- front/components/ChatList/index.tsx
+++ front/components/ChatList/index.tsx
@@ -1,18 +1,23 @@
 import { ChatZone } from "./styels";
-import React, {VFC} from "react";
+import React, {useCallback, useRef, VFC} from "react";
 import Chat from '@components/Chat';
 import {IDM} from '@typings/db'
+import {Scrollbars} from 'react-custom-scrollbars'
 
 interface Props {
   chatData ?: IDM[];
 }
 
 const ChatList :VFC<Props> = ({ chatData }) => {
+  const scrollbarRef = useRef(null)
+  const onScroll = useCallback(()=> {}, [])
   return (
     <ChatZone>
-      {chatData?.map((chat)=> (
-        <Chat key={chat.id} data={chat}/>
-      ))}
+      <Scrollbars autoHide ref={scrollbarRef} onScrollFrame={onScroll}>
+        {chatData?.map((chat)=> (
+          <Chat key={chat.id} data={chat}/>
+        ))}
+      </Scrollbars>
     </ChatZone>
   )
 }
