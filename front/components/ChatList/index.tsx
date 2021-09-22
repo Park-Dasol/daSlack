11d42c8d2ec3d7073a13fe2aef1f54db57628cc5
dryans1 <dryans@naver.com>
2021-09-23 02:06:42 +0900

    feat : 리버스 인피니트 스크롤

--- front/components/ChatList/index.tsx
+++ front/components/ChatList/index.tsx
@@ -1,19 +1,27 @@
 import { ChatZone, Section, StickyHeader } from "./styels";
-import React, {useCallback, useRef, VFC} from "react";
+import React, {useCallback, useRef, VFC, forwardRef} from "react";
 import Chat from '@components/Chat';
-import {IDM} from '@typings/db'
+import {IDM, IChat} from '@typings/db'
 import {Scrollbars} from 'react-custom-scrollbars'
 
 interface Props {
   chatSections : {[key:string]: IDM[]};
+  setSize : (f:(size: number)=> number) => Promise<(IDM| IChat)[][]| undefined>;
+  isEmpty : boolean;
+  isReachingEnd : boolean;
 }
+// const scrollbarRef = useRef(null)
 
-const ChatList :VFC<Props> = ({ chatSections }) => {
-  const scrollbarRef = useRef(null)
-  const onScroll = useCallback(()=> {}, [])
+const ChatList = forwardRef<Scrollbars, Props>(({ chatSections , setSize, isReachingEnd, isEmpty}, ref ) => {
+  const onScroll = useCallback((values)=> {
+    if (values.scrollTop === 0 && !isReachingEnd){
+      //데이터를 추가로 로딩하기
+      setSize((prevSize) => prevSize + 1).then(()=> { })
+    }
+  }, [])
   return (
     <ChatZone>
-      <Scrollbars autoHide ref={scrollbarRef} onScrollFrame={onScroll}>
+      <Scrollbars autoHide ref={ref} onScrollFrame={onScroll}>
         {Object.entries(chatSections).map(([date, chats])=> {
           return (
             <Section className={`section-${date}`} key={date}>
@@ -32,6 +40,6 @@ const ChatList :VFC<Props> = ({ chatSections }) => {
       </Scrollbars>
     </ChatZone>
   )
-}
+})
 
 export default ChatList
--- front/pages/DirectMessage/index.tsx
+++ front/pages/DirectMessage/index.tsx
@@ -1,5 +1,5 @@
 import { Container,Header } from '@pages/Channel/styles'
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useState, useRef } from 'react'
 import gravatar from 'gravatar'
 import useSWR, { useSWRInfinite } from 'swr'
 import { useParams } from 'react-router'
@@ -10,16 +10,26 @@ import useInput from '@hooks/useInput'
 import axios from 'axios'
 import { IDM } from '@typings/db'
 import makeSection from '@utils/makeSection'
+import Scrollbars from 'react-custom-scrollbars';
+
 
 const DirectMessage = () => {
   const {workspace, id} = useParams<{workspace: string; id: string}>();
   const {data: userData} = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher)
   const {data: myData}  = useSWR(`/api/users`, fetcher)
   const [chat, onChangeChat, setChat] = useInput('')
-  const {data: chatData, mutate: matateChat, revalidate} = useSWR<IDM[]>(
-     `/api/workspaces/${workspace}/dms/${id}/chats?perPage=20&page=1`,
+  
+  const {data: chatData, mutate: matateChat, revalidate, setSize} = useSWRInfinite<IDM[]>(
+    (index)=> `/api/workspaces/${workspace}/dms/${id}/chats?perPage=20&page=${index +1}`,
     fetcher,
   )
+  //infinite scrolling 에 필요한것
+  // 데이터 요청했는데 더이상 가져올 데이터가 없을경우
+  const isEmpty = chatData?.[0]?.length === 0; 
+  // 데이터를 요청했는데, 데이터가 있긴 하는데 가져오는 크기보다 작은 경우
+  const isReachingEnd = isEmpty || (chatData&& chatData[chatData.length - 1]?.length < 20) || false; 
+
+  const scrollbarRef = useRef<Scrollbars>(null);
 
   const onSubmitForm = useCallback((e)=> {
     e.preventDefault();
@@ -42,7 +52,7 @@ const DirectMessage = () => {
     return null;
   }
 
-  const chatSections = makeSection(chatData? [...chatData].reverse() : [])
+  const chatSections = makeSection(chatData? chatData.flat().reverse() : [])
 
   return ( 
     <Container>
@@ -50,7 +60,7 @@ const DirectMessage = () => {
         <img src={gravatar.url(userData.email, {s: '24px', d: 'retro'})} alt={userData.nickname} />
         <span>{userData.nickname}</span>
       </Header>
-      <ChatList chatSections={chatSections}/>
+      <ChatList chatSections={chatSections} ref={scrollbarRef} setSize={setSize} isEmpty={isEmpty} isReachingEnd={isReachingEnd}/>
       <ChatBox chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm}/>
     </Container>
   )
